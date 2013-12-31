404a27e2fe30d98fa5ff8ce81f877a7028f8c9c5
Gauge <Gauger16@gmail.com>
2013-12-30 17:53:14 -0800

    game now initializes, lets players toggle in and out of the locked status and moves on to the next turn

--- Source/Game.hx
+++ Source/Game.hx
@@ -3,14 +3,56 @@ package;
 class Game {
 
 	// four character per team
-	var team1:Array;
-	var team2:Array;
+	var team1:Array <Character>;
+	var team2:Array <Character>;
 
 	// counts the number of turns the game has been going on for
 	var turn:Int;
-	// 
-	var playerOneLockedIn:Bool;
-	var playerTwoLockedIn:Bool;
+	var gamestate:Int;
 
-	function new(){}
+	var player1LockedIn:Bool;
+	var player2LockedIn:Bool;
+
+	function new() {
+		gamestate = Globals.GAME_INIT;
+		// create a new team of characters
+		team1 = [new Character(), new Character(), new Character(), new Character()];
+		team2 = [new Character(), new Character(), new Character(), new Character()];
+
+		turn = 1;
+		player1LockedIn = false;
+		player2LockedIn = false;
+
+		gamestate = Globals.Game_TURN;
+	}
+
+	// toggles the locked in state of the player that calls it
+	// it also proforms the check to move to the next turn
+	public function lockin(player:Int) {
+		if (player == Globals.PLAYER_ONE){
+			player1LockedIn = !player1LockedIn;
+		
+		} else if (player == Globals.PLAYER_TWO) {
+			player2LockedIn = !player2LockedIn;
+		}
+
+		if (player1LockedIn && player2LockedIn) {
+			updateGame();
+		}
+	}
+
+	// this is where all the calculations for battle will happen
+	// and all the after math will be updated
+	private function updateGame() {
+		gamestate = Globals.GAME_UPDATE;
+
+		
+		newTurn();
+	}
+
+	private function newTurn() {
+		player1LockedIn = false;
+		player2LockedIn = false;
+		turn++;
+	}
 }
