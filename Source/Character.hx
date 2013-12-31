7e88cf530f4679f9d7c14ecd6bb3da4989b5490f
grantopher <shadowcarson@gmail.com>
2013-12-30 16:40:14 -0800

    added the first few variables and such

--- Source/Character.hx
+++ Source/Character.hx
@@ -1,6 +1,13 @@
 package;
 
 class Character {
-
+	public var level:Int;
+	public var currentXp:Int;
+	public var health:Int;
+	public var healthMax:Int;
+	public var physicalPower:Int;
+	public var armor:Int;
+	public var statusEffects:Array <Dynamic>;
+	
 	function new() {}
 }
