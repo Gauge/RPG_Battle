19c0c48d152487c95fca1094bcc1b445e3967bfb
grantopher <shadowcarson@gmail.com>
2014-01-03 16:07:07 -0800

    character sprite loading is handled through seperate class file

--- Source/GameGraphics.hx
+++ Source/GameGraphics.hx
@@ -18,24 +18,6 @@ class  GameGraphics extends Sprite {
 	static var LEFT = 0;
 	static var RIGHT = 1;
 
-	static var CHAR_NAME = 0;
-	static var CHAR_BITMAP_LOC = 1;
-	static var ANIM_LIST_START = 2;
-
-	static var ANIM_NAME = 0;
-	static var ANIM_LOOP = 1;
-	static var FRAME_LIST_START = 2;
-
-	static var FRAME_DURATION = 0;
-	static var FRAME_X = 1;
-	static var FRAME_Y = 2;
-
-	static var FRAME_WIDTH = 3;
-	static var FRAME_HEIGHT = 4;
-	static var FRAME_REF_X = 5;
-	static var FRAME_REF_Y = 6;
-	static var FRAME_ID = 7;
-
 	public var characterList:Array <CharacterSprite>;
 	public var characterData:Array <Dynamic>;
 	public var displayContainer:Sprite;
@@ -54,11 +36,10 @@ class  GameGraphics extends Sprite {
 	public function init(){
 		gameStage = Lib.current.stage;
 		characterList = new Array();
-		characterData = FileLoader.loadData('assets/dataTest.xml');
 		getScreenDimentions();
 		loadBackdrop();
-
-		loadCharacterData();
+		var spriteLoader = new LoadCharacterSprite();
+		characterList = spriteLoader.loadSprites();
 
 		//FileLoader.loadXmlFile('assets/dataTest.xml');
 
@@ -89,10 +70,6 @@ class  GameGraphics extends Sprite {
 		
 	}
 
-	public function drawFrame() : Void {
-
-	}
-
 	private function resizeSprite( original:Sprite, targetDimentions:Rectangle ) : Void {
 		original.width = targetDimentions.width;
 		original.height = targetDimentions.height;
@@ -100,68 +77,7 @@ class  GameGraphics extends Sprite {
 		original.y = targetDimentions.y;
 	}
 
-	private function loadCharacterData() : Void {
-		for( c in 0...characterData.length ) {
-			var character = new CharacterSprite(); 
 
-			character.direction = LEFT;
-			character.tilesheet = loadTilesheet(Assets.getBitmapData(characterData[c][CHAR_BITMAP_LOC]), characterData[c]);
-			character.animationList = loadAnimations( characterData[c] );
-			character.currentAnimation = character.animationList[0];
-
-			characterList.push(character);
-		}
-	}
-
-	private function loadTilesheet( bitmap : BitmapData, characterData : Array <Dynamic> ) : Tilesheet {
-		var tilesheet = new Tilesheet(bitmap);
-
-		for( animations in ANIM_LIST_START...characterData.length ) {
-			for( frames in FRAME_LIST_START...characterData[animations].length ) {
-
-				var frame = characterData[animations][frames];
-				var geometry = new Rectangle(frame[FRAME_X], frame[FRAME_Y], frame[FRAME_WIDTH], frame[FRAME_HEIGHT]);
-				var reference = new Point(frame[FRAME_REF_X], frame[FRAME_REF_Y]);
-
-				tilesheet.addTileRect(geometry, reference);
-			}
-		}
-		
-		return tilesheet;
-	}
-
-	private function loadAnimations( data : Array <Dynamic> ) : Array <Animation> {
-  		var animationlist = new Array(); 
-
-		for(a in ANIM_LIST_START...data.length){
-			var animation = new Animation();
-			animation.name = data[a][ANIM_NAME];
-			animation.loop = data[a][ANIM_LOOP];
-			animation.frameList = loadFrameData(data[a]);
-
-			animation.currentFrameId = 0;
-			animation.timer = 0;
-			
-			animationlist.push(animation);
-		}
-
-		return animationlist;
-	}
-
-	private function loadFrameData( data : Array <Dynamic> ) : Array <Frame> {
-		var framelist = new Array();
-
-		for (f in FRAME_LIST_START...data.length) {
-			var frame = new Frame();
-			frame.tileId = data[f][FRAME_ID];
-			frame.duration = data[f][FRAME_DURATION];
-
-			framelist.push(frame);
-		}
-
-
-		return framelist;
-	}
 
 	private function renderLoop( event:Event ) : Void {
 		displayContainer.graphics.clear();
---
+++ Source/LoadCharacterSprite.hx
@@ -0,0 +1,107 @@
+package;
+
+import flash.display.Sprite;
+import flash.display.Bitmap;
+import flash.display.BitmapData;
+import flash.geom.Rectangle;
+import flash.geom.Point;
+import flash.events.Event;
+import openfl.Assets;
+import flash.Lib;
+import Animations;
+import FileLoader;
+import openfl.display.Tilesheet;
+
+class LoadCharacterSprite {
+
+	static var LEFT = 0;
+	static var RIGHT = 1;
+
+	static var CHAR_NAME = 0;
+	static var CHAR_BITMAP_LOC = 1;
+	static var ANIM_LIST_START = 2;
+
+	static var ANIM_NAME = 0;
+	static var ANIM_LOOP = 1;
+	static var FRAME_LIST_START = 2;
+
+	static var FRAME_DURATION = 0;
+	static var FRAME_X = 1;
+	static var FRAME_Y = 2;
+
+	static var FRAME_WIDTH = 3;
+	static var FRAME_HEIGHT = 4;
+	static var FRAME_REF_X = 5;
+	static var FRAME_REF_Y = 6;
+	static var FRAME_ID = 7;
+
+	public function new(){}
+
+	public function loadSprites() : Array <CharacterSprite> {
+		var characterList:Array <CharacterSprite> = new Array();
+		var characterData = FileLoader.loadData('assets/dataTest.xml');
+		for( c in 0...characterData.length ) {
+			var character = new CharacterSprite(); 
+
+			character.direction = LEFT;
+			character.tilesheet = loadTilesheet(Assets.getBitmapData(characterData[c][CHAR_BITMAP_LOC]), characterData[c]);
+			character.animationList = loadAnimations( characterData[c] );
+			character.currentAnimation = character.animationList[0];
+
+			characterList.push(character);
+		}
+
+		return characterList;
+	}
+
+	private function loadTilesheet( bitmap : BitmapData, characterData : Array <Dynamic> ) : Tilesheet {
+		var tilesheet = new Tilesheet(bitmap);
+
+		for( animations in ANIM_LIST_START...characterData.length ) {
+			for( frames in FRAME_LIST_START...characterData[animations].length ) {
+
+				var frame = characterData[animations][frames];
+				var geometry = new Rectangle(frame[FRAME_X], frame[FRAME_Y], frame[FRAME_WIDTH], frame[FRAME_HEIGHT]);
+				var reference = new Point(frame[FRAME_REF_X], frame[FRAME_REF_Y]);
+
+				tilesheet.addTileRect(geometry, reference);
+			}
+		}
+		
+		return tilesheet;
+	}
+
+	private function loadAnimations( data : Array <Dynamic> ) : Array <Animation> {
+  		var animationlist = new Array(); 
+
+		for(a in ANIM_LIST_START...data.length){
+			var animation = new Animation();
+			animation.name = data[a][ANIM_NAME];
+			animation.loop = data[a][ANIM_LOOP];
+			animation.frameList = loadFrameData(data[a]);
+
+			animation.currentFrameId = 0;
+			animation.timer = 0;
+			
+			animationlist.push(animation);
+		}
+
+		return animationlist;
+	}
+
+	private function loadFrameData( data : Array <Dynamic> ) : Array <Frame> {
+		var framelist = new Array();
+
+		for (f in FRAME_LIST_START...data.length) {
+			var frame = new Frame();
+			frame.tileId = data[f][FRAME_ID];
+			frame.duration = data[f][FRAME_DURATION];
+
+			framelist.push(frame);
+		}
+
+
+		return framelist;
+	}
+
+}
