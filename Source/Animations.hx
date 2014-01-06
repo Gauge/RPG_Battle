975cde1a8e5fd121b28c3bb5827becf3c55a03f6
grantopher <shadowcarson@gmail.com>
2014-01-05 22:35:21 -0800

    Menu functionality added, user can now select actions through the menu

--- Source/Animations.hx
+++ Source/Animations.hx
@@ -67,11 +67,36 @@ class ActionMenu extends Sprite {
 	public var show:Bool;
 	public var target:CharacterSprite;
 	public var tilesheet:Tilesheet;
+	public var selection:String;
 
 	public function new(){
 		super();
 		tilesheet = new Tilesheet(Assets.getBitmapData("assets/menusprite.png"));
 		tilesheet.addTileRect(new Rectangle(0,0,50,100), new Point(25,100));
+	}
+
+	public function on_click(event : MouseEvent){
+		var y = Math.abs(event.localY);
+		var x = event.localX / Math.abs(event.localX); //30, 100, 160
+
+		var call_menu_select = new Event("menu_select", true);
+		if(y < 30){
+			selection = (x > 0) ? "item" : "exit";
+			event.target.dispatchEvent(call_menu_select);
+		}
+		else if(y < 100) {
+			selection = "magic";
+			event.target.dispatchEvent(call_menu_select);
+		}
+		else if(y < 160) {
+			selection = "defend";
+			event.target.dispatchEvent(call_menu_select);
+		}
+		else{
+			selection = "attack";
+			event.target.dispatchEvent(call_menu_select);
+		}
+
 	}
 
 }
--- Source/GameGraphics.hx
+++ Source/GameGraphics.hx
@@ -54,6 +54,8 @@ class  GameGraphics extends Sprite {
 
 	var game:Game;
 	var actionmenu:ActionMenu;
+
+	var selectingTarget:Bool;
 	
 	function new(){
 
@@ -64,13 +66,10 @@ class  GameGraphics extends Sprite {
 		init();
 		gameStage.addEventListener (KeyboardEvent.KEY_DOWN, keyDown);
 		gameStage.addEventListener ('select_character', sendCharSelect);
+		gameStage.addEventListener ('menu_select', menuSelect);
 	}
 
-	private function sendCharSelect(event:Event){
-		game.selectCharacter(event.target.team, event.target.id);
-		actionmenu.target = event.target;
-		actionmenu.show = true;
-	}
+
 
 	public function init(){
 		gameStage = Lib.current.stage;
@@ -84,11 +83,46 @@ class  GameGraphics extends Sprite {
 		loadBackdrop();
 		loadContainers();
 
+		selectingTarget = false;
+
 		actionmenu = new ActionMenu();
+		actionmenu.addEventListener(MouseEvent.MOUSE_DOWN, actionmenu.on_click);
 		gameStage.addChild(actionmenu);
+
 
 		gameStage.addEventListener(Event.ENTER_FRAME, renderLoop);
 
+	}
+
+	private function menuSelect(event : Event) {
+		var target = event.target;
+		var selection = target.selection;
+		switch (selection) {
+			case "exit" :
+				actionmenu.show = false;
+			case "item":
+				actionmenu.show = false;
+			case "magic":
+				selectingTarget = true;
+			case "defend":
+				game.selectAction(Globals.PLAYER_TWO, Globals.ACTION_DEFEND, null, null);
+				actionmenu.show = false;
+			case "attack":
+				selectingTarget = true;
+			}
+	}
+
+	private function sendCharSelect(event:Event){
+		if(event.target.team == 2 && selectingTarget == false){
+			game.selectCharacter(event.target.team, event.target.id);
+			actionmenu.target = event.target;
+			actionmenu.show = true;
+		}
+		else if(event.target.team == 1 && selectingTarget == true) {
+			game.selectAction(Globals.PLAYER_TWO, Globals.ACTION_ATTACK, event.target.id, Globals.PLAYER_ONE);
+			selectingTarget = false;
+			actionmenu.show = false;
+		}
 	}
 
 	private function loadBackdrop() : Void {
@@ -174,50 +208,50 @@ class  GameGraphics extends Sprite {
 	private function keyDown(event:KeyboardEvent):Void {
 		switch (event.keyCode) {
 			case Keyboard.NUMBER_1:
-				game.lockin(Globals.PLAYER_ONE);
+			game.lockin(Globals.PLAYER_ONE);
 
 			case Keyboard.NUMBER_2:
-				game.lockin(Globals.PLAYER_TWO);
+			game.lockin(Globals.PLAYER_TWO);
 
 			case Keyboard.Q:
-				game.selectCharacter(Globals.PLAYER_ONE, Globals.CHARACTER_1);
+			game.selectCharacter(Globals.PLAYER_ONE, Globals.CHARACTER_1);
 
 			case Keyboard.W:
-				game.selectCharacter(Globals.PLAYER_ONE, Globals.CHARACTER_2);
+			game.selectCharacter(Globals.PLAYER_ONE, Globals.CHARACTER_2);
 
 			case Keyboard.E:
-				game.selectCharacter(Globals.PLAYER_ONE, Globals.CHARACTER_3);
+			game.selectCharacter(Globals.PLAYER_ONE, Globals.CHARACTER_3);
 
 			case Keyboard.R:
-				game.selectCharacter(Globals.PLAYER_ONE, Globals.CHARACTER_4);
+			game.selectCharacter(Globals.PLAYER_ONE, Globals.CHARACTER_4);
 
 			case Keyboard.T:
-				game.selectCharacter(Globals.PLAYER_TWO, Globals.CHARACTER_1);
+			game.selectCharacter(Globals.PLAYER_TWO, Globals.CHARACTER_1);
 
 			case Keyboard.Y:
-				game.selectCharacter(Globals.PLAYER_TWO, Globals.CHARACTER_2);
+			game.selectCharacter(Globals.PLAYER_TWO, Globals.CHARACTER_2);
 
 			case Keyboard.U:
-				game.selectCharacter(Globals.PLAYER_TWO, Globals.CHARACTER_3);
+			game.selectCharacter(Globals.PLAYER_TWO, Globals.CHARACTER_3);
 
 			case Keyboard.I:
-				game.selectCharacter(Globals.PLAYER_TWO, Globals.CHARACTER_4);
+			game.selectCharacter(Globals.PLAYER_TWO, Globals.CHARACTER_4);
 
 			case Keyboard.O:
-				game.selectAction(Globals.PLAYER_ONE, Globals.ACTION_ATTACK, Globals.PLAYER_TWO, Globals.CHARACTER_3);
+			game.selectAction(Globals.PLAYER_ONE, Globals.ACTION_ATTACK, Globals.PLAYER_TWO, Globals.CHARACTER_3);
 
 			case Keyboard.P:
-				game.selectAction(Globals.PLAYER_ONE, Globals.ACTION_DEFEND, Globals.PLAYER_ONE, Globals.CHARACTER_1);
+			game.selectAction(Globals.PLAYER_ONE, Globals.ACTION_DEFEND, Globals.PLAYER_ONE, Globals.CHARACTER_1);
 
 			case Keyboard.A:
-				game.selectAction(Globals.PLAYER_TWO, Globals.ACTION_ATTACK, Globals.PLAYER_ONE, Globals.CHARACTER_3);
+			game.selectAction(Globals.PLAYER_TWO, Globals.ACTION_ATTACK, Globals.PLAYER_ONE, Globals.CHARACTER_3);
 
 			case Keyboard.S:
-				game.selectAction(Globals.PLAYER_TWO, Globals.ACTION_DEFEND, Globals.PLAYER_ONE, Globals.CHARACTER_1);
+			game.selectAction(Globals.PLAYER_TWO, Globals.ACTION_DEFEND, Globals.PLAYER_ONE, Globals.CHARACTER_1);
 
 			case Keyboard.ESCAPE:
-				trace("quiting program");
-				System.exit(0);
+			trace("quiting program");
+			System.exit(0);
 
 		}
 	}
