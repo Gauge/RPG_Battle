018ca85323bdfc279c1a4be68b54d73fcd70b9c0
Gauge <Gauger16@gmail.com>
2014-07-13 19:07:45 -0700

    added health bar. added Action menu. modifyed sizing and other random stuff.

--- Source/Graphics/Screens/ActiveGame.hx
+++ Source/Graphics/Screens/ActiveGame.hx
@@ -13,8 +13,10 @@ import flash.geom.Rectangle;
 import graphics.uicomponents.Background;
 import graphics.uicomponents.LockinButton;
 import graphics.uicomponents.CharacterSprite;
+import graphics.uicomponents.ActionMenu;
 
 import motion.Actuate;
+import motion.easing.Quad;
 
 class ActiveGame extends Sprite {
 	// holds the game instance
@@ -23,6 +25,7 @@ class ActiveGame extends Sprite {
 	// graphic components
 	var background:Background;
 	var lockinButton:LockinButton;
+	var actionMenu:ActionMenu;
 	var team1:Array<CharacterSprite>;
 	var team2:Array<CharacterSprite>;
 	
@@ -45,10 +48,12 @@ class ActiveGame extends Sprite {
 		initBackground();
 		initLockinButton();
 		initCharacters();
+		initActionMenu();
 
 		trace("Initializing render loop");
 		// adjusts the screen if orientation changes
 		addEventListener("lockin", onLockinClick);
+		addEventListener("character_select", onCharacterSelect);
 		Lib.current.stage.addEventListener(Event.RESIZE, onScreenResize);
 		Lib.current.stage.addEventListener(Event.ENTER_FRAME, enterFrame);
 
@@ -67,6 +72,10 @@ class ActiveGame extends Sprite {
 		for (i in 0...4) {
 			var char = Loader.loadSprite("box", Globals.LEFT, i);
 			this.addChild(char);
+			var player = game.getPlayerById(Globals.PLAYER_ONE);
+			var max = player.team[i].getMaxVitality();
+			var vit = player.team[i].getVitality();
+			char._init_(max, vit);
 			char.recalculateSize();
 			team1.push(char);
 		}
@@ -74,14 +83,13 @@ class ActiveGame extends Sprite {
 		for (i in 0...4) {
 			var char = Loader.loadSprite("box", Globals.RIGHT, i);
 			this.addChild(char);
+			var player = game.getPlayerById(Globals.PLAYER_TWO);
+			var max = player.team[i].getMaxVitality();
+			var vit = player.team[i].getVitality();
+			char._init_(max, vit);
 			char.recalculateSize();
 			team2.push(char);
 		}
-	}
-
-	// NOTE: i think i will include this in the init characters
-	private function initHpBars(): Void {
-		trace("creating hp bars");
 	}
 
 	private function initLockinButton(): Void {
@@ -92,6 +100,8 @@ class ActiveGame extends Sprite {
 
 	private function initActionMenu(): Void {
 		trace("creating action menu");
+		actionMenu = new ActionMenu();
+		addChild(actionMenu);
 	}
 
 	private function onScreenResize(e:Event) {
@@ -106,11 +116,28 @@ class ActiveGame extends Sprite {
 		}
 	}
 
+	private function onCharacterSelect(e:Event) {
+		var player = ((e.target.getDirection() == Globals.LEFT) ? Globals.PLAYER_ONE : Globals.PLAYER_TWO);
+		var character = e.target.getCharacterNumber();
+		game.selectCharacter(player, character);
+		actionMenu.showActionMenu();
+	}
+
 	private function onLockinClick(e:Event) {
 		game.selectCharacter(Globals.PLAYER_ONE, Globals.CHARACTER_1); // Sample data
 		game.selectAction(Globals.PLAYER_ONE, Globals.ACTION_ATTACK, Globals.PLAYER_TWO, Globals.CHARACTER_3); // Sample data
 		game.lockin(Globals.PLAYER_ONE);
 		game.lockin(Globals.PLAYER_TWO);
+		
+
+	}
+
+	private function initSequence() {
+
+	}
+
+	private function updateSequence() {
+
 	}
 
 	private function enterFrame(e:Event){
@@ -125,11 +152,14 @@ class ActiveGame extends Sprite {
 
 	private function render() {
 		lockinButton.render();
+		actionMenu.render();
 		for (char in team1) { 
 			char.render();
+			char.update(1);
 		}
 
 		for (char in team2) {
+			char.update(1);
 			char.render();
 		}
 	}
